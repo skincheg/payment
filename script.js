a548e6b43ff0bf234a3313c36cabdaa187edbf36
skincheg <53021132+skincheg@users.noreply.github.com>
2019-07-30 17:59:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const firstCard = document.querySelector('.first-card')
+const secondCard = document.querySelector('.second-card')
+
+firstCard.addEventListener('click', cardClick);
+secondCard.addEventListener('click', cardClick);
+
+function cardClick() {
+    firstCard.classList.toggle('first-card__active');
+    secondCard.classList.toggle('second-card__active');
+}
