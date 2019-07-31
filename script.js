ca987bdb8cf8ae7790476387210aad399e6af69a
skincheg <53021132+skincheg@users.noreply.github.com>
2019-07-31 23:20:27 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,35 @@ const firstCard = document.querySelector('.first-card')
 const secondCard = document.querySelector('.second-card')
 
 firstCard.addEventListener('click', cardClick);
-secondCard.addEventListener('click', cardClick);
+//secondCard.addEventListener('click', cardClick);
 
 function cardClick() {
     firstCard.classList.toggle('first-card__active');
     secondCard.classList.toggle('second-card__active');
 }
+
+
+
+
+
+const cardSelectYandex = document.querySelector('#card-select__item-yandex')
+const cardSelectVisa = document.querySelector('#card-select__item-visa')
+const cardSelectBitcoin = document.querySelector('#card-select__item-bitcoin')
+
+cardSelectYandex.addEventListener('click', () => {
+    cardSelectVisa.classList.remove('card-select__item-visa')
+    cardSelectBitcoin.classList.remove('card-select__item-bitcoin')
+    cardSelectYandex.classList.toggle('card-select__item-yandex')
+})
+
+cardSelectVisa.addEventListener('click', () => {
+    cardSelectBitcoin.classList.remove('card-select__item-bitcoin')
+    cardSelectYandex.classList.remove('card-select__item-yandex')
+    cardSelectVisa.classList.toggle('card-select__item-visa')
+})
+
+cardSelectBitcoin.addEventListener('click', () => {
+    cardSelectYandex.classList.remove('card-select__item-yandex')
+    cardSelectVisa.classList.remove('card-select__item-visa')
+    cardSelectBitcoin.classList.toggle('card-select__item-bitcoin')
+})
